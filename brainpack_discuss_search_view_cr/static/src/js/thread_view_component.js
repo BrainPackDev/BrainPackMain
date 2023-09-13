86adc49ad7a532759cb150cd6b44c9eaf8227613
MayurDuran <mayur@duran.digital>
2023-09-13 10:09:29 +0530

    [add] custom modules added

---
+++ brainpack_discuss_search_view_cr/static/src/js/thread_view_component.js
@@ -0,0 +1,144 @@
+ /** @odoo-module **/
+import { ThreadView } from '@mail/components/thread_view/thread_view';
+import { patch } from 'web.utils';
+
+patch(ThreadView.prototype, 'brainpack_discuss_search_view_cr.thread_view_component', {
+//     _onKeyUpSearch(ev) {
+//         const query = ev.target.value.trim().toLowerCase();
+//         var domain = ['|',['subject','ilike',query],['body','ilike',query]]
+//         this.threadView.update({
+//          stringifiedDomain: JSON.stringify(domain),
+//        });
+//        this.threadView._onChangeStringifiedDomain()
+//    }
+    _onKeyUpSearch(ev) {
+        const query = ev.target.value.trim().toLowerCase()
+        var domain = ['|',['subject','ilike',query],['body','ilike',query]]
+
+        if(query != ''){
+            this.threadView.update({
+              searchMessage: true,
+              searchString: ev.target.value,
+              upDisable:false,
+              downDisable:false,
+            });
+        }
+        else{
+            this.threadView.update({
+              searchMessage: false,
+              upDisable:true,
+              downDisable:true,
+              searchString: ev.target.value,
+            });
+        }
+        this.threadView.update({
+          stringifiedDomain: JSON.stringify(domain),
+        });
+        this.threadView._onChangeStringifiedDomain()
+    },
+    _onClickUp(){
+        var self = this
+        var nextMessage = 0
+        if(this.threadView.numberOfSearch){
+            nextMessage = this.threadView.currentSearchCount + 1
+        }
+
+        if(nextMessage >= this.threadView.numberOfSearch){
+            this.threadView.update({
+              upDisable: true,
+              downDisable:false
+            });
+        }
+        else if(nextMessage < this.threadView.numberOfSearch){
+            this.threadView.update({
+              downDisable: false,
+            });
+        }
+        if(nextMessage > this.threadView.numberOfSearch){
+            return false
+        }
+
+        this.threadView.update({
+          currentSearchCount: nextMessage,
+        });
+
+        this.messaging.discuss.update({
+          currentSearchCount: nextMessage,
+        });
+
+        if(nextMessage > 0){
+            this.threadView.update({
+              searchMessageId : this.threadView.threadViewer.threadCache.SearchMessages[nextMessage-1].id,
+            });
+        }
+        else{
+            this.threadView.update({
+              searchMessageId : false,
+            });
+        }
+
+        if(this.threadView.searchMessageId){
+            setTimeout(function(){
+                if($('.o_Message[data-id='+self.threadView.searchMessageId+']').length > 0){
+                    $('.o_ThreadView_messageList').animate({
+                        scrollTop: $('.o_Message[data-id='+self.threadView.searchMessageId+']')[0].offsetTop - 100 ,
+                    }, 400);
+                }
+                else{
+                    self.threadView.update({searchUpDown:true})
+                    $.blockUI()
+                    self.threadView.threadViewer.threadCache.loadMoreMessages();
+                }
+            }, 1000);
+        }
+    },
+    _onClickDown(){
+        var self = this
+        var nextMessage = 0
+        if(this.threadView.numberOfSearch){
+            nextMessage = this.threadView.currentSearchCount - 1
+            if(nextMessage == 0){
+                return false
+            }
+            if(nextMessage == 1){
+                this.threadView.update({
+                  upDisable: false,
+                  downDisable:true
+                });
+            }
+            else if(nextMessage > 1){
+                this.threadView.update({
+                  upDisable: false,
+                });
+            }
+        }
+        this.threadView.update({
+          currentSearchCount: nextMessage,
+        });
+
+        if(nextMessage > 0){
+            this.threadView.update({
+              searchMessageId : this.threadView.threadViewer.threadCache.SearchMessages[nextMessage-1].id,
+            });
+        }
+        else{
+            this.threadView.update({
+              searchMessageId : false,
+            });
+        }
+        if(this.threadView.searchMessageId){
+            setTimeout(function(){
+                if($('.o_Message[data-id='+self.threadView.searchMessageId+']').length > 0){
+                    $('.o_ThreadView_messageList').animate({
+                        scrollTop: $('.o_Message[data-id='+self.threadView.searchMessageId+']')[0].offsetTop - 100 ,
+                    }, 200);
+                }
+                else{
+                    self.threadView.update({searchUpDown:true})
+                    $.blockUI()
+                    self.threadView.threadViewer.threadCache.loadMoreMessages();
+                }
+            }, 700);
+        }
+    }
+});
