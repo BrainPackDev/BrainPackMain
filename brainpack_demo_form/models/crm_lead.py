c6bba17403cb6b24c8f128ad98665a86c6e054b5
MayurDuran <mayur@duran.digital>
2023-09-25 22:35:54 +0530

    [add] changes added

--- brainpack_demo_form/models/crm_lead.py
+++ brainpack_demo_form/models/crm_lead.py
@@ -4,3 +4,12 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     try_demo = fields.Boolean(string='Try Demo',default=False)
+
+    @api.model
+    def create(self, vals):
+        res = super().create(vals)
+        if res.try_demo and res.type == 'lead':
+            mail_template = self.env.ref('brainpack_demo_form.mail_template_create_demo_mail_template').id
+            template = self.env['mail.template'].browse(mail_template)
+            template.send_mail(res.id, force_send=True)
+        return res
