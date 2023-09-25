a52f3dc59127f5d03359c9ccc34f11c0d5717ca5
MayurDuran <mayur@duran.digital>
2023-09-25 18:44:50 +0530

    [add] BrainPack Demo Form added

---
+++ brainpack_demo_form/models/crm_lead.py
@@ -0,0 +1,6 @@
+from odoo import api, SUPERUSER_ID, fields, models, modules, tools, _
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    try_demo = fields.Boolean(string='Try Demo',default=False)
