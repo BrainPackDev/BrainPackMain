1c2efcf34c25325d4fabdc5f366003d833de01a3
MayurDuran <mayur@duran.digital>
2023-10-03 20:00:16 +0530

    [add] duran task for helpdesk ticket

---
+++ brainpack_helpdesk_mgmt/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'BrainPack Helpdesk Management',
+    'version': '16.0.0.0',
+    'author': 'BrainPack',
+    'website': 'https://www.brainpack.io',
+    'license': 'LGPL-3',
+    'sequence': 2,
+    'images': [],
+    'summary': """
+    """,
+    'depends': ['helpdesk_mgmt'],
+    'data': [
+        'data/mail_template.xml',
+        'views/res_company_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'description': """
+    """,
+}
