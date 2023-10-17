6078e94a9bb1e28ef417982c27f2425c0f72e864
MayurDuran <mayur@duran.digital>
2023-10-17 17:20:33 +0530

    [add] fix issue

--- brainpack_meta_whatsapp/models/res_users.py
+++ brainpack_meta_whatsapp/models/res_users.py
@@ -9,29 +9,30 @@ class ResUsers(models.Model):
 
     provider_id = fields.Many2one('provider', 'Provider', domain="[('company_id', 'in', company_ids)]")
     provider_ids = fields.Many2many('provider', 'provider_rel', 'provider_id', 'pid', domain="[('company_id', 'in', company_ids)]", string='Providers')
-    @api.constrains('provider_id', 'provider_ids', 'company_ids', 'company_id')
-    def _check_company_providers(self):
-        all_provider_by_company = self.env['provider'].sudo().search([('company_id', 'in', self.company_ids.ids)])
 
-        provider_not_allowed = []
-        for i in self.provider_ids:
-            if i not in all_provider_by_company:
-                provider_not_allowed.append(i.name)
-
-        if provider_not_allowed and any(provider_not_allowed):
-            raise ValidationError(
-                _('For field Allowed Providers %(provider_name)s is not in the allowed providers for user %(user_name)s (%(provider_allowed)s).',
-                  provider_name= ', '.join(provider_not_allowed),
-                  user_name=self.name,
-                  provider_allowed=', '.join(all_provider_by_company.mapped('name')))
-            )
-        if self.provider_id.id not in all_provider_by_company.ids:
-            raise ValidationError(
-            _('For field Default Provider %(provider_name)s is not in the allowed providers for user %(user_name)s (%(provider_allowed)s).',
-              provider_name=self.provider_id.name,
-              user_name=self.name,
-              provider_allowed=', '.join(all_provider_by_company.mapped('name')))
-            )
+    # @api.constrains('provider_id', 'provider_ids', 'company_ids', 'company_id')
+    # def _check_company_providers(self):
+    #     all_provider_by_company = self.env['provider'].sudo().search([('company_id', 'in', self.company_ids.ids)])
+    #
+    #     provider_not_allowed = []
+    #     for i in self.provider_ids:
+    #         if i not in all_provider_by_company:
+    #             provider_not_allowed.append(i.name)
+    #
+    #     if provider_not_allowed and any(provider_not_allowed):
+    #         raise ValidationError(
+    #             _('For field Allowed Providers %(provider_name)s is not in the allowed providers for user %(user_name)s (%(provider_allowed)s).',
+    #               provider_name= ', '.join(provider_not_allowed),
+    #               user_name=self.name,
+    #               provider_allowed=', '.join(all_provider_by_company.mapped('name')))
+    #         )
+    #     if self.provider_id.id not in all_provider_by_company.ids:
+    #         raise ValidationError(
+    #         _('For field Default Provider %(provider_name)s is not in the allowed providers for user %(user_name)s (%(provider_allowed)s).',
+    #           provider_name=self.provider_id.name,
+    #           user_name=self.name,
+    #           provider_allowed=', '.join(all_provider_by_company.mapped('name')))
+    #         )
     def write(self, values):
         values = self._remove_reified_groups(values)
         if values.get('groups_id'):
