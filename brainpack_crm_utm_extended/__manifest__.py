909859e911e68dd6461ea8af726002ac8571370a
MayurDuran <mayur@duran.digital>
2023-10-30 15:39:26 +0530

    [add] brainpack_crm_utm_extended module added

---
+++ brainpack_crm_utm_extended/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'BrainPack CRM UTM Package',
+    'version': '16.0.0.1',
+    'author': 'BrainPack',
+    'website': 'https://www.brainpack.io',
+    'license': 'LGPL-3',
+    'sequence': 2,
+    'images': [],
+    'summary': """
+    """,
+    'depends': ['crm','brainpack_website_extended'],
+    'data': [
+        'views/crm_view.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+        ],
+    },
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'description': """
+    """,
+}
