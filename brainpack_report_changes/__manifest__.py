703686d3e9a5524c288e05401c11452047d8c529
MayurDuran <mayur@duran.digital>
2023-11-02 11:50:09 +0530

    [add] product name bold added on portal side on sale order and subscription

--- brainpack_report_changes/__manifest__.py
+++ brainpack_report_changes/__manifest__.py
@@ -8,12 +8,14 @@
     'images': [],
     'summary': """
     """,
-    'depends': ['sale','account'],
+    'depends': ['sale','account','sale_subscription'],
     'data': [
         'data/report_data.xml',
         'views/res_company.xml',
         'views/ir_actions_report_templates.xml',
         'views/report_invoice.xml',
+        'views/sale_portal.xml',
+        'views/subscription_portal_template.xml',
     ],
     'assets': {
         'web.assets_frontend': [
