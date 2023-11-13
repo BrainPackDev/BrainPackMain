11d66f95ebb88d825ebafaffa51ba0ba9c4c0052
MayurDuran <mayur@duran.digital>
2023-11-13 14:53:35 +0530

    [add] push notification condition added

--- push_notifications/models/mail_channel.py
+++ push_notifications/models/mail_channel.py
@@ -14,30 +14,31 @@ class Channel(models.Model):
         res = super()._channel_message_notifications(
             message, message_format=message_format
         )
+        account_id = self.env['push.account'].search([('is_mobile_app_notification', '=', True)],limit=1)
+        if account_id.app_firebase_admin_key_file:
+            partners =  self.channel_partner_ids.ids
+            if message.author_id and message.author_id.id in partners:
+                partners.remove(message.author_id.id)
+            if partners:
+                domain= []
+                domain.append(('partner_id', 'in', partners))
+                domain.append(('fcm_token', '!=', False))
 
-        partners =  self.channel_partner_ids.ids
-        if message.author_id and message.author_id.id in partners:
-            partners.remove(message.author_id.id)
-        if partners:
-            domain= []
-            domain.append(('partner_id', 'in', partners))
-            domain.append(('fcm_token', '!=', False))
+                if domain:
+                    user_ids = self.env['res.users'].sudo().search(domain)
+                if user_ids:
+                    notification_vals = {
+                        'message_id': message.id,
+                        'app_notification_title': message.author_id.name,
+                        'message': html2text.html2text(message.body),
+                        'users_ids': [(6,0,user_ids.ids)],
+                        'total_users':len(user_ids.ids),
+                        'post_method': 'now'
+                    }
+                    if message.attachment_ids:
+                        notification_vals.update({'message':notification_vals.get('message') + "\n" + ", ".join([attachment.name for attachment in message.attachment_ids])})
 
-            if domain:
-                user_ids = self.env['res.users'].sudo().search(domain)
-            if user_ids:
-                notification_vals = {
-                    'message_id': message.id,
-                    'app_notification_title': message.author_id.name,
-                    'message': html2text.html2text(message.body),
-                    'users_ids': [(6,0,user_ids.ids)],
-                    'total_users':len(user_ids.ids),
-                    'post_method': 'now'
-                }
-                if message.attachment_ids:
-                    notification_vals.update({'message':notification_vals.get('message') + "\n" + ", ".join([attachment.name for attachment in message.attachment_ids])})
-
-                notification = self.env['app.push.notification'].sudo().create(notification_vals)
-                notification.send_notitfcation()
+                    notification = self.env['app.push.notification'].sudo().create(notification_vals)
+                    notification.send_notitfcation()
 
         return res
